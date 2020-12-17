72111dcff2d753c975c7c0c954d8e28f7be203e8
Ilnur786 <Ilnur-qwerty@mail.ru>
2020-12-17 16:48:20 +0300

    image generator with functions

--- generator.php
+++ generator.php
@@ -10,34 +10,38 @@ $w = (int)mysqli_fetch_row($width_array)[0];
 $sql2 = "SELECT height FROM sizes1 WHERE code= '{$size}'";
 $height_array = mysqli_query($link, $sql2);
 $h = (int)mysqli_fetch_row($height_array)[0];
-print gettype($h);
-print $h;
-print $name;
-print $size;
-//    print __DIR__.'/gallery/.' . $name . '.jpg';
-//    $image = new Thumbs(__DIR__.'\gallery\.' . $name . '.jpg');
-//    $image->resize($width, $height);
-//    $image->save(__DIR__.'\cache\
-$filename = __DIR__ ."\gallery\\{$name}.jpg";
-print $filename;
 
-$info   = getimagesize($filename);
-var_dump($info);
-$width  = $info[0];
-$height = $info[1];
-$img = imagecreatefromjpeg($filename);
-$tmp = imageCreateTrueColor($w, $h);
-
-imageCopyResampled($tmp, $img, 0, 0, 0, 0, $w, $h, $width, $height);
-
-$hash_dir_name = md5($name);
-$filename = __DIR__."/cache/{$hash_dir_name}/{$w}{$h}.jpg";
-
-if (!file_exists($filename))
+function doImagePreview($name, $width_new, $height_new)
 {
-    imagejpeg($tmp, $filename, 100);
-
+    $hash_dir_name = md5($name);
+    if (!file_exists(__DIR__.'\cache\\' . $hash_dir_name))
+    {
+        mkdir(__DIR__.'\cache\\' . $hash_dir_name);
+    }
+    $filename_preview = __DIR__."\cache\\{$hash_dir_name}\\{$width_new}{$height_new}.jpg";
+    $filename_original = __DIR__ ."\gallery\\{$name}.jpg";
+    $info   = getimagesize($filename_original);
+    $width_original  = $info[0];
+    $height_original = $info[1];
+    $img = imagecreatefromjpeg($filename_original);
+    $tmp = imageCreateTrueColor($width_new, $height_new);
+    imageCopyResampled($tmp, $img, 0, 0, 0, 0, $width_new, $height_new, $width_original, $height_original);
+    imagejpeg($tmp, $filename_preview, 100);
+    imagedestroy($tmp);
+    return $filename_preview;
 }
+function getImagePreview($name, $width_new, $height_new)
+{
+    $hash_dir_name = md5($name);
+    $filename_preview = __DIR__."\cache\\{$hash_dir_name}\\{$width_new}{$height_new}.jpg";
+    if (!file_exists($filename_preview))
+    {
+        $filename_preview = doImagePreview($name, $width_new, $height_new);
+    }
+    return $filename_preview;
+}
+
+print getImagePreview($name, $w, $h);
 
 ?>
 
