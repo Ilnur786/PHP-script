b8d4e13fb00677017bb1c3b9f4d3d7202bc1b116
Ilnur786 <Ilnur-qwerty@mail.ru>
2020-12-24 20:38:41 +0300

    errors are caught now

--- generator.php
+++ generator.php
@@ -4,7 +4,7 @@ require 'connection.php';
 
 $name = $_GET['name'];
 $size = $_GET['size'];
-//$name = 'Безымянный.png';
+//$name = 'img.jpg';
 //$size = 'min';
 $sql1 = $pdo->prepare("SELECT width, height FROM sizes1 WHERE code = :size");
 $sql1->execute(array(':size' => $size));
@@ -12,11 +12,20 @@ $row = $sql1->fetch();
 $w = (int)$row['width'];
 $h = (int)$row['height'];
 
-function fileBuildPath(...$segments): string
-{
+$cache_ext = fileBuildPath(__DIR__, 'cache_ext');
+if (!file_exists($cache_ext)) {
+    mkdir($cache_ext);
+}
+
+function fileBuildPath(...$segments): string {
     return join(DIRECTORY_SEPARATOR, $segments);
 }
 
+/**
+ * @param $filepath
+ * @return GdImage|resource
+ * @throws Exception
+ */
 function imageCreateFromAny($filepath) {
     $type = exif_imagetype($filepath);
 
@@ -32,46 +41,72 @@ function imageCreateFromAny($filepath) {
             break;
     }
 
-    if (!$im) {                 #какую то проверку добавить
-        return false;
+    if (!$im) {
+        throw new Exception("Не удалось открыть изображение. Возможно, файл не является одним из типов: GIF, JPEG/JPG, PNG");
     }
     return $im;
 }
 
-function doImagePreview($name, $width_new, $height_new): string
-{
+function doImagePreview($name, $width_new, $height_new): string {
     $hash_dir_name = md5($name);
     $dir_path = fileBuildPath('cache_ext', $hash_dir_name);
-    if (!file_exists($dir_path))   #проверка на существование дериктории с изображениями разных размеров
-    {
+    if (!file_exists($dir_path)) { #проверка на существование дериктории с изображениями разных размеров
+
         mkdir($dir_path);
     }
     $filename_preview = fileBuildPath(__DIR__, 'cache_ext', $hash_dir_name, $width_new . $height_new . '.jpg');
     $filename_original = fileBuildPath('gallery', $name);
+    if (!file_exists($filename_original) or !is_readable($filename_original)) {
+         throw new Exception("файл {$name} не существует или не доступен");
+    }
+    if (!in_array(mime_content_type($filename_original), ['image/gif', 'image/jpeg', 'image/png'])) {
+        throw new Exception("файл {$name} не является изображением");
+    }
     $info = getimagesize($filename_original);
+    if (!$info) {
+        throw new Exception("файл {$name} не существует или не доступен");
+    }
     $width_original  = $info[0];
     $height_original = $info[1];
-    $img = imageCreateFromAny($filename_original);
+    try {
+        $img = imageCreateFromAny($filename_original);
+    } catch (Exception $e) {
+        echo 'Выброшено исключение: ',  $e->getMessage(), "\n";
+    }
     $tmp = imageCreateTrueColor($width_new, $height_new);
     imageCopyResampled($tmp, $img, 0, 0, 0, 0, $width_new, $height_new, $width_original, $height_original);
     imagejpeg($tmp, $filename_preview, 100);
     imagedestroy($tmp);
     return $filename_preview;
 }
-function getImagePreview($name, $width_new, $height_new): string
-{
+
+function getImagePreview($name, $width_new, $height_new): string {
     $hash_dir_name = md5($name);
     $filename_preview = fileBuildPath(__DIR__, 'cache_ext', $hash_dir_name, $width_new . $height_new . '.jpg');
     if (!file_exists($filename_preview))   #проверка на существование изображения с необходимыми размерами
     {
-        $filename_preview = doImagePreview($name, $width_new, $height_new);
+        try {
+            $filename_preview = doImagePreview($name, $width_new, $height_new);
+        } catch (Exception $e) {
+            echo 'Выброшено исключение: ',  $e->getMessage(), "\n";
+        }
     }
     return $filename_preview;
 }
 
 $url = getImagePreview($name, $w, $h);
-header('Content-type: image/jpeg');
-readfile($url);
+
+try {
+    if (!file_exists($url)) {
+        throw new Exception("не удалось сгенерировать изображение из {$name}");
+    }
+    header('Content-type: image/jpeg');
+    readfile($url);
+}
+
+catch (Exception $e) {
+    echo 'Выброшено исключение: ',  $e->getMessage(), "\n";
+}
 //header('Content-type: text/plane');
 //echo $url;
 
