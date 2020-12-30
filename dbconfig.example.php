c3a815014b02857d1ab0e5810d25b0be676ef1bb
Ilnur786 <Ilnur-qwerty@mail.ru>
2020-12-30 20:46:46 +0300

    ...

--- dbconfig.example.php
+++ dbconfig.example.php
@@ -1,15 +1,15 @@
 <?php
-//$user = $_ENV['USER'];
-//$pass = $_ENV['PASS'];
-//$host = $_ENV['HOST'];
-//$dbname = $_ENV['DB_NAME'];
+$user = $_ENV['USER'];
+$pass = $_ENV['PASS'];
+$host = $_ENV['HOST'];
+$dbname = $_ENV['DB_NAME'];
 
-$url = getenv('JAWSDB_MARIA_URL');
-$dbparts = parse_url($url);
-
-$host = $dbparts['host'];
-$user = $dbparts['user'];
-$pass = $dbparts['pass'];
-$dbname = ltrim($dbparts['path'],'/');
+//$url = getenv('JAWSDB_MARIA_URL');
+//$dbparts = parse_url($url);
+//
+//$host = $dbparts['host'];
+//$user = $dbparts['user'];
+//$pass = $dbparts['pass'];
+//$dbname = ltrim($dbparts['path'],'/');
 
 $dsn = "mysql:host={$host};dbname={$dbname}";
